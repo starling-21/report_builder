b1354ebc0358cb3cac6eabf317132fddfd421e12
starling <starling1715@gmail.com>
2019-12-24 18:18:02 +0200

    integrated select2 to edit chain page

--- reports/views.py
+++ reports/views.py
@@ -33,58 +33,58 @@ def serviceman_list_view(request):
     return render(request, 'reports/serviceman_list.html', context)
 
 
-def edit_service_members_chain_view(request, serviceman_id):
-    """change servicemen chain if needed"""
-    swap_id = None
-    if request.method == 'POST':
-        serviceman_chain = request.session['serviceman_chain']
-        if 'edit_chain_id' in request.POST:
-            print("Editing_member with id:", request.POST.get('edit_chain_id'))
-            swap_id = int(request.POST.get('edit_chain_id'))
-
-        elif 'submit_new_id' in request.POST:
-            old_id = request.POST.get('submit_new_id')
-            swap_id = request.POST.get('swap_id')
-            print('id swap: {} -> {}'.format(old_id, swap_id))
-            for member in serviceman_chain:
-                if member.id == int(old_id):
-                    replace_index = serviceman_chain.index(member)
-            # change position to temporary and unit
-            initial_member_position = request.session['initial_serviceman_chain'][replace_index].position
-            initial_member_position.temp_supervisor = True
-            initial_member_unit = request.session['initial_serviceman_chain'][replace_index].unit
-            # replace member in chain
-            new_serviceman = Serviceman.objects.get(id=int(swap_id))
-            new_serviceman.position = initial_member_position
-            new_serviceman.unit = initial_member_unit
-            serviceman_chain[replace_index] = new_serviceman
-
-            request.session['serviceman_chain'] = serviceman_chain
-            request.session.modified = True
-        elif 'remove_chain_id' in request.POST:
-            remove_id = request.POST.get('remove_chain_id')
-            for member in serviceman_chain:
-                if member.id == int(remove_id):
-                    serviceman_chain.remove(member)
-            request.session['serviceman_chain'] = serviceman_chain
-            request.session.modified = True
-        elif 'submit_chain_editing' in request.POST:
-            print("submit_chain_editting")
-            return redirect(reverse('reports:reports_list', kwargs={'serviceman_id': serviceman_id}))
-
-    elif request.method == 'GET':
-        serviceman = Serviceman.objects.get(id=serviceman_id)
-        serviceman_chain = report_content_util.get_servicemen_chain_list(serviceman)
-        request.session['serviceman_chain'] = serviceman_chain
-        request.session['initial_serviceman_chain'] = serviceman_chain
-        request.session.modified = True
-
-    serviceman_chain = request.session['serviceman_chain']
-    context = {
-        'serviceman_chain': serviceman_chain,
-        'swap_id': swap_id,
-    }
-    return render(request, 'reports/edit_service_members_chain.html', context)
+# def edit_service_members_chain_view(request, serviceman_id):
+#     """change servicemen chain if needed"""
+#     swap_id = None
+#     if request.method == 'POST':
+#         serviceman_chain = request.session['serviceman_chain']
+#         if 'edit_chain_id' in request.POST:
+#             print("Editing_member with id:", request.POST.get('edit_chain_id'))
+#             swap_id = int(request.POST.get('edit_chain_id'))
+#
+#         elif 'submit_new_id' in request.POST:
+#             old_id = request.POST.get('submit_new_id')
+#             swap_id = request.POST.get('swap_id')
+#             print('id swap: {} -> {}'.format(old_id, swap_id))
+#             for member in serviceman_chain:
+#                 if member.id == int(old_id):
+#                     replace_index = serviceman_chain.index(member)
+#             # change position to temporary and unit
+#             initial_member_position = request.session['initial_serviceman_chain'][replace_index].position
+#             initial_member_position.temp_supervisor = True
+#             initial_member_unit = request.session['initial_serviceman_chain'][replace_index].unit
+#             # replace member in chain
+#             new_serviceman = Serviceman.objects.get(id=int(swap_id))
+#             new_serviceman.position = initial_member_position
+#             new_serviceman.unit = initial_member_unit
+#             serviceman_chain[replace_index] = new_serviceman
+#
+#             request.session['serviceman_chain'] = serviceman_chain
+#             request.session.modified = True
+#         elif 'remove_chain_id' in request.POST:
+#             remove_id = request.POST.get('remove_chain_id')
+#             for member in serviceman_chain:
+#                 if member.id == int(remove_id):
+#                     serviceman_chain.remove(member)
+#             request.session['serviceman_chain'] = serviceman_chain
+#             request.session.modified = True
+#         elif 'submit_chain_editing' in request.POST:
+#             print("submit_chain_editting")
+#             return redirect(reverse('reports:reports_list', kwargs={'serviceman_id': serviceman_id}))
+#
+#     elif request.method == 'GET':
+#         serviceman = Serviceman.objects.get(id=serviceman_id)
+#         serviceman_chain = report_content_util.get_servicemen_chain_list(serviceman)
+#         request.session['serviceman_chain'] = serviceman_chain
+#         request.session['initial_serviceman_chain'] = serviceman_chain
+#         request.session.modified = True
+#
+#     serviceman_chain = request.session['serviceman_chain']
+#     context = {
+#         'serviceman_chain': serviceman_chain,
+#         'swap_id': swap_id,
+#     }
+#     return render(request, 'reports/edit_service_members_chain.html', context)
 
 
 def reports_list_view(request, serviceman_id):
@@ -135,38 +135,91 @@ def return_report_document_view(request):
 
 def member_search_view(request):
     print("Search request, method", request.method)
+    service_members_list = []
     if request.method == 'POST':
         for k, v in request.POST.items():
             print("{}:{}".format(k, v))
         filter_param = request.POST.get('filter_param')
-        service_members_list = []
+
         if filter_param is not None and len(filter_param) > 0:
-            # filter_param = "-1"
             query_set = Serviceman.objects.filter(
                 Q(first_name__icontains=filter_param) |
                 Q(last_name__icontains=filter_param) |
                 Q(rank__name__icontains=filter_param)
-                # Q(first_name__startswith=filter_param) |
-                # Q(last_name__startswith=filter_param) |
-                # Q(rank__name__startswith=filter_param)
             )
 
             for member in query_set:
                 service_members_list.append(
                     {'id': member.id, 'text': member.rank.__str__() + ' ' + member.get_first_last_name()})
             if len(service_members_list) > 0:
-                result = json.dumps(service_members_list)
-                return HttpResponse(result)
+                return HttpResponse(json.dumps(service_members_list))
         else:
-            service_members_list.append({'id': -1, 'text': '----------'})
-            return HttpResponse(json.dumps(service_members_list))
-    elif request.is_ajax():
-        print("AJAX")
+            # service_members_list.append({'id': -1, 'text': 'Пошук (Імена та Прізвища з Великої літери)'})
+            service_members_list.append({'id': -1, 'text': 'Нічого не знайдено'})
 
-    if request.method == 'GET':
-        service_members_list = Serviceman.objects.all()
+            #return all objects
+            query_set = Serviceman.objects.all()
+            for member in query_set:
+                service_members_list.append(
+                    {'id': member.id, 'text': member.rank.__str__() + ' ' + member.get_first_last_name()})
+            return HttpResponse(json.dumps(service_members_list))
 
     context = {
         'service_members_list': service_members_list,
     }
     return render(request, 'reports/test_search_user.html', context)
+
+
+def edit_service_members_chain_view(request, serviceman_id):
+    """change servicemen chain if needed"""
+    swap_id = None
+    if request.method == 'POST':
+        serviceman_chain = request.session['serviceman_chain']
+        if 'edit_chain_id' in request.POST:
+            print("Editing_member with id:", request.POST.get('edit_chain_id'))
+            swap_id = int(request.POST.get('edit_chain_id'))
+
+        elif 'submit_new_id' in request.POST:
+            old_id = request.POST.get('submit_new_id')
+            swap_id = request.POST.get('swap_id')
+            if old_id != swap_id:
+                print('id swap: {} -> {}'.format(old_id, swap_id))
+                for member in serviceman_chain:
+                    if member.id == int(old_id):
+                        replace_index = serviceman_chain.index(member)
+                # change position to temporary and unit
+                initial_member_position = request.session['initial_serviceman_chain'][replace_index].position
+                initial_member_position.temp_supervisor = True
+                initial_member_unit = request.session['initial_serviceman_chain'][replace_index].unit
+                # replace member in chain
+                new_serviceman = Serviceman.objects.get(id=int(swap_id))
+                new_serviceman.position = initial_member_position
+                new_serviceman.unit = initial_member_unit
+                serviceman_chain[replace_index] = new_serviceman
+
+                request.session['serviceman_chain'] = serviceman_chain
+                request.session.modified = True
+        elif 'remove_chain_id' in request.POST:
+            remove_id = request.POST.get('remove_chain_id')
+            for member in serviceman_chain:
+                if member.id == int(remove_id):
+                    serviceman_chain.remove(member)
+            request.session['serviceman_chain'] = serviceman_chain
+            request.session.modified = True
+        elif 'submit_chain_editing' in request.POST:
+            print("submit_chain_editting")
+            return redirect(reverse('reports:reports_list', kwargs={'serviceman_id': serviceman_id}))
+
+    elif request.method == 'GET':
+        serviceman = Serviceman.objects.get(id=serviceman_id)
+        serviceman_chain = report_content_util.get_servicemen_chain_list(serviceman)
+        request.session['serviceman_chain'] = serviceman_chain
+        request.session['initial_serviceman_chain'] = serviceman_chain
+        request.session.modified = True
+
+    serviceman_chain = request.session['serviceman_chain']
+    context = {
+        'serviceman_chain': serviceman_chain,
+        'swap_id': swap_id,
+    }
+    return render(request, 'reports/edit_service_members_chain.html', context)
