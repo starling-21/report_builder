d9af34a7c04a603bda35e6b9beb51701d2f914ed
Vasiliy <starling1715@gmail.com>
2020-08-05 01:25:43 +0300

    releases 1 (Dockerized, Django 3.1, no debug)

--- app/military_site/urls.py
+++ app/military_site/urls.py
@@ -25,14 +25,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
-
-
 #Add Django site authentication urls (for login, logout, password management)
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns = [
+#         path('__debug__/', include(debug_toolbar.urls)),
+#     ] + urlpatterns
